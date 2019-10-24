620e1bc4b5c5ce7629379b69a78c06a9b0415cb1
Ramon Moraes <ramon.moraes94@gmail.com>
2019-10-24 19:29:19 -0300

    add some comments to basecaly illustrate the simplest layout

--- neuralnetwork/multilayer.py
+++ neuralnetwork/multilayer.py
@@ -1,4 +1,12 @@
 class Multilayer:
+"""
+    I1 - H1
+            - o
+    I2 - H2
+---------------------
+    | I1*W1 I2*W1 |  -\  | H1 |
+    | I1*W2 I2*W2 |  -/  | H2 |
+"""
     def __init__(self, inputs, hidden, output):
         self.inputs = inputs
         self.hidden = hidden
