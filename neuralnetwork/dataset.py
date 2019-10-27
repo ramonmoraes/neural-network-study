10ee27cdf0e4b1d3839b5ee9a26aee839bc3c1d7
Ramon Moraes <ramon.moraes94@gmail.com>
2019-10-27 12:09:09 -0300

    add MnistDataset

--- neuralnetwork/dataset.py
+++ neuralnetwork/dataset.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Dataset:
     def __init__(self, inputs, outputs):
         self.inputs = inputs
@@ -29,3 +32,33 @@ FIVE_OR_MORE_DATASET = Dataset(
 )
 
 XOR_DATASET = Dataset([[0, 0], [0, 1], [1, 0], [1, 1]], [1, 0, 0, 1])
+
+
+class MnistDataset(Dataset):
+    file_path = "dataset/mnist_train.csv"
+
+    def __init__(self):
+        super().__init__([], [])
+
+    def load(self):
+        print("[Loading MnistDataset...]")
+        with open(self.file_path, "r") as f:
+            csv_reader = csv.reader(f, delimiter=",")
+            read_header = False
+            for row in csv_reader:
+                if not read_header:
+                    read_header = True
+                    continue
+
+            self.inputs.append(self.format_input(row[1:]))
+            self.outputs.append(self.numeral_to_output(row[0]))
+        print("[Loaded]")
+
+    def numeral_to_output(self, numeral: str):
+        number = int(numeral)
+        inpt = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        inpt[number + 1] = 1
+        return inpt
+
+    def format_input(self, output):
+        return [int(x)/255 for x in output]
