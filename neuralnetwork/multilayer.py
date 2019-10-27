14cc8d87887ec1f3aa5986e0a4f5a8293b2db351
Ramon Moraes <ramon.moraes94@gmail.com>
2019-10-27 16:31:29 -0300

    multilayer creating array sizes

--- neuralnetwork/multilayer.py
+++ neuralnetwork/multilayer.py
@@ -1,14 +1,6 @@
 class Multilayer:
-"""
-    I1 - H1
-            - o
-    I2 - H2
----------------------
-    | I1*W1 I2*W1 |  -\  | H1 |
-    | I1*W2 I2*W2 |  -/  | H2 |
-"""
-    def __init__(self, inputs, hidden, output):
-        self.inputs = inputs
-        self.hidden = hidden
-        self.output = output
+    def __init__(self, inputs_size, hidden_size, output_size):
+        self.inputs = inputs_size * [0]
+        self.hidden = hidden_size * [0]
+        self.output = output_size * [0]
 
