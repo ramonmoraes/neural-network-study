2e1d172cce8ce2a4cd3fecdc0cc11838911dc81c
Ramon Moraes <ramon.moraes94@gmail.com>
2019-10-28 09:06:24 -0300

    add scatter 3d

--- util/plot.py
+++ util/plot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def scatter_2d(dataset):
     def scatter_value_color(value, color):
@@ -13,4 +14,26 @@ def scatter_2d(dataset):
     scatter_value_color(1, "b")
     scatter_value_color(0, "r")
     plt.show()
+
+
+def scatter_3d(dataset):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    def scatter_value_color(value, color):
+        x = []
+        y = []
+        z = []
+        for (inpt, output) in dataset:
+            if output == value:
+                x.append(inpt[0])
+                y.append(inpt[1])
+                z.append(inpt[2])
+        import pdb; pdb.set_trace()
+        ax.scatter3D(x,y,z, color=color)
+    
+    scatter_value_color(1, "b")
+    scatter_value_color(0, "r")
+    plt.show()
+
 
