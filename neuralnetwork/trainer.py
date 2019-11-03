8553f89ef2caaea5d4d6607353869bdb169f4329
Ramon Moraes <ramon.moraes94@gmail.com>
2019-11-03 19:55:40 -0300

    Implement absctrac Trainer logic (#6)
    * implement Trainer and Trainable
    
    * Perceptron extending from Trainable

---
+++ neuralnetwork/trainer.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+class Trainer:
+    def __init__(self, dataset, trainable):
+        self.dataset = dataset
+        self.trainable = trainable
+
+    def train_times(self, times=1):
+        for i in range(times):
+            for inpt, output in self.dataset:
+                self.trainable.train(inpt, output)
+
+    def analyze(self):
+        print("[Analyzing]")
+        errors = []
+        for inpt, output in self.dataset:
+            predicted = self.trainable.predict(inpt)
+            print(f"expected: {output} got:{predicted}")
+            errors.append(0 if output == predicted else 1)
+        print(f"Error: {np.mean(errors)}")
+
+        self.trainable.explain()
+
+
+class Trainable:
+    def predict(self, inputs):
+        raise NotImplementedError()
+
+    def train(self, inputs, outputs):
+        raise NotImplementedError()
+
+    def explain(self):
+        raise NotImplementedError()
+
