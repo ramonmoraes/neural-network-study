3ad80bd2f37bdf8f6ccf33bf01c9eb657ea4c8d3
Ramon Moraes <ramon.moraes94@gmail.com>
2019-11-10 15:19:09 -0300

    add predict test

--- tests/neuralnetwork/test_multillayer.py
+++ tests/neuralnetwork/test_multillayer.py
@@ -1,4 +1,5 @@
 from neuralnetwork.multilayer import Multilayer
+import numpy as np
 
 
 def test_mlp_link():
@@ -9,3 +10,16 @@ def test_mlp_link():
     assert mlp.layers[2].previous_layer is mlp.layers[1]
     assert mlp.layers[1].previous_layer is mlp.layers[0]
 
+
+def test_predict():
+    mlp = Multilayer(2, 2, 1)
+    mlp.layers[0].bias = 1
+    mlp.layers[1].bias = 1
+
+    mlp.layers[0].forward_weights = np.array([[0.5, 0.4], [0.2, 0.04]])
+    mlp.layers[1].forward_weights = np.array([[0.2], [0.8]])
+    mlp.link_layers()
+
+    prediction = mlp.predict([1,1]) 
+    assert len(prediction) == 1
+    assert round(prediction[0],2) == 0.96
